c9931aa58dbfc2e1101f8a5e2d9fb6789fbdfd77
alfieuku <alfieukfr@gmail.com>
2025-03-21 19:00:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__, static_folder='static')
 
@@ -6,8 +7,7 @@ app = Flask(__name__, static_folder='static')
 def index():
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    # Render sets the PORT environment variable automatically
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
